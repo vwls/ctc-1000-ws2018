fc6197b0784d6d73c3b7bbaf7aefe23d72f50da9
Cissy9951 <35778450+Cissy9951@users.noreply.github.com>
2018-02-05 13:14:27 +0800

    Delete config.js

--- assignment_1/cissy_luo/final/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	consumer_key:         '0z5PaeeHLjXr2VMazeeFIYtUf',
- 	consumer_secret:      'jg8vyvcAhhbZq46GYR4YgPKFPh98xObahEtSgChAA6VGADdo1P',
-	access_token:         '2282739985-SGvi7ateL7Jl7om15uytaGiGPBpmiBsOUBedQ6B',
-	access_token_secret:  'nGjuvXn79LUMixcYFem8HMVUsfjrI0tu51bSvpFqTMRcr',
-  	timeout_ms:           60*1000, 
-}
